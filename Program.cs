5cd66a58470b6fc998afa4a0608f7ca3fb9f9939
kavita-abjima <128472387+kavita-abjima@users.noreply.github.com>
2023-04-06 18:48:39 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Hello";
+            int i, j;
+            for (i = 0; i < test.Length; i++)
+            {
+                for (j = i; j < test.Length; j++)
+                {
+                    Console.Write(test.Substring(i, (j - i) + 1) + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+/*output
+H, He, Hel, Hell, Hello 
+e, el, ell, ello
+l, ll, llo
+l, lo
+o */
